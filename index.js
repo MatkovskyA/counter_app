482423a3e013bbb7647fb790752df7ed6de06362
MatkovskyA <a.matkovsky14@gmail.com>
2023-05-02 14:42:39 +0300

    Адаптив для мобильного

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log (1);
+let button = document.querySelector(".button__plus");
+let button2 = document.querySelector(".button__reset");
+let counterText = document.querySelector(".counter__meaning");
+let counter = 0;
+
+button.addEventListener("click", function() {
+    counter = counter + 1;
+    counterText.innerText = counter;
+});
+
+button2.addEventListener("click", function () {
+    counter = 0;
+    counterText.innerText = counter;
+})
+
+
